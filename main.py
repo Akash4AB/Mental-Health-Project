62c979d7dec5de1a8bc4a4f944f4180325a9d2f5
Akash Kumar <165520140+Akash4AB@users.noreply.github.com>
2024-08-14 12:37:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,162 @@
+import streamlit as st
+import streamlit.components.v1 as stc
+
+from eda import ds_eda_app
+from dataprep import ds_dataprep_app
+from ml_app import ds_ml_app
+
+home_temp = """
+            <div style="background-color:#0e0f3f;padding:1px;border-radius:25px">
+		    <h2 style="color:white;font-family:calibri;text-align:center;">MENTAL HEALTH - DATA SCIENCE </h2>
+            </div>
+            """
+home2_temp = """
+            <style>
+                    h1 {
+                        font-size: 18px;
+                        color: white;
+                    }
+                    .font-big {
+                        font-size: large;
+                    }
+                    .font-small {
+                        font-size: middle;
+                    }
+                </style>
+
+            <style type="text/css">
+                    .left    { text-align: left;}
+                    .right   { text-align: right;}
+                    .center  { text-align: center;}
+                    .justify-italic { text-align: justify;font-style:italic;line-height:1.2;}
+                  ul.justify-bullet { text-align: justify;list-style-position: outside;line-height:1.3}
+                  ol.justify-numbering { text-align: justify;list-style-position: outside;line-height:1.4}
+            </style>
+            """
+about_temp = """
+            <div style="background-color:#0e0f3f;padding:1px;border-radius:25px">
+		    <h2 style="color:white;font-family:calibri;text-align:center;">MENTAL HEALTH TREATMENT PREDICTION SYSTEM </h2>
+            </div>
+            """
+desc_about_temp = """
+                <style>
+                    h1 {
+                        font-size: 18px;
+                        color: white;
+                    }
+                    .font-big {
+                        font-size: large;
+                    }
+                    .font-small {
+                        font-size: middle;
+                    }
+                </style>
+
+                <style type="text/css">
+                    .left    { text-align: left;}
+                    .right   { text-align: right;}
+                    .center  { text-align: center;}
+                    .justify-italic { text-align: justify;font-style:italic;line-height:1.2;}
+                  ul.justify-bullet { text-align: justify;list-style-position: outside;line-height:1.3}
+                  ol.justify-numbering { text-align: justify;list-style-position: outside;line-height:1.4}
+                </style>
+
+                <body>
+                        <p> Data Source :
+                            <a href="https://www.kaggle.com/code/numbrami/mental-health-treatment-prediction#Random-Forest-Classifier" title = "Menuju ke link data source"
+                            >https://www.kaggle.com/code</a>
+                        </p>
+                    <h1 class='font-big'>About Dataset</h1>
+                        <p class="justify-italic">"Mental Health in Tech Survey" is the dataset that was collected in 2014. This data is the result of the survey that measures 
+                                                    attitude toward mental health and frequency of mental illness disorders in one of the tech workplace.</p>
+                    <h1 class='font-big'>Research Question</h1>
+                        <p class="left">The aim of our study is to answer the below research questions:</p>
+                    <ul class="justify-bullet">
+                        <li>How does the frequency of mental illness vary by geographic location ?</li>
+                        <li>How does the attitude of the company toward mental health in the highest number of employees having mental disorder country ?</li>
+                        <li>What are the strongest predictors of mental health illness or certain attitudes towards mental health in the workplace ?</li>
+                    </ul>
+                    <h1 class='font-big'>Feature</h1>
+                        <p class="left">This dataset contains the following data :</p>
+                    <ol class="justify-numbering">
+                        <li>Timestamp</li>
+                        <li>Age</li>
+                        <li>Gender</li>
+                        <li>Country</li>
+                        <li>State : <i>If you live in the India, which state or territory do you live in?</i></li>
+                        <li>self_employed : <i>Are you self-employed?</i></li>
+                        <li>family_history : <i>Do you have a family history of mental illness?</i></li>
+                        <li>treatment : <i>Have you sought treatment for a mental health condition?</i></li>
+                        <li>work_interfere : <i>If you have a mental health condition, do you feel that it interferes with your work?</i></li>
+                        <li>no_employees : <i>How many employees does your company or organization have ?</i></li>
+                        <li>remote_work : <i>Do you work remotely (outside of an office) at least 50% of the time ?</i></li>
+                        <li>tech_company : <i>Is your employer primarily a tech company/organization ?</i></li>
+                        <li>benefits : <i>Does your employer provide mental health benefits ?</i></li>
+                        <li>care_options : <i>Do you know the options for mental health care your employer provides ?</i></li>
+                        <li>wellness_program : <i>Has your employer ever discussed mental health as part of an employee wellness program ?</i></li>
+                        <li>seek_help : <i>Does your employer provide resources to learn more about mental health issues and how to seek help ?</i></li>
+                        <li>anonymity : <i>Is your anonymity protected if you choose to take advantage of mental health or substance abuse treatment resources ?</i></li>
+                        <li>leave : <i>How easy is it for you to take medical leave for a mental health condition ?</i></li>
+                        <li>mental_health_consequence : <i>Do you think that discussing a mental health issue with your employer would have negative consequences ?</i></li>
+                        <li>phys_health_consequence : <i>Do you think that discussing a physical health issue with your employer would have negative consequences ?</i></li>
+                        <li>coworkers : <i>Would you be willing to discuss a mental health issue with your coworkers ?</i></li>
+                        <li>supervisor : <i>Would you be willing to discuss a mental health issue with your direct supervisor(s) ?</i></li>
+                        <li>mental_health_interview : <i>Would you bring up a mental health issue with a potential employer in an interview ?</i></li>
+                        <li>phys_health_interview : <i>Would you bring up a physical health issue with a potential employer in an interview ?</i></li>
+                        <li>mental_vs_physical : <i>Do you feel that your employer takes mental health as seriously as physical health ?</i></li>
+                        <li>obs_consequence : <i>Have you heard of or observed negative consequences for coworkers with mental health conditions in your workplace ?</i></li>
+                        <li>comments : <i>Any additional notes or comments ?</i></li>
+                    </ul>
+                </body>
+                 """
+prep_temp = """
+            <div style="background-color:#0e0f3f;padding:1px;border-radius:25px">
+		    <h2 style="color:white;font-family:calibri;text-align:center;">DATA PREPROCESSING AND CLEANSING </h2>
+            </div>
+            """
+eda_temp = """
+            <div style="background-color:#0e0f3f;padding:1px;border-radius:25px">
+		    <h2 style="color:white;font-family:calibri;text-align:center;">EXPLORATORY DATA ANALYSIS (EDA) </h2>
+            </div>
+            """
+ml_temp = """
+            <div style="background-color:#0e0f3f;padding:1px;border-radius:25px">
+		    <h2 style="color:white;font-family:calibri;text-align:center;">MACHINE LEARNING</h2>
+            </div>
+            """
+
+
+def main_section():
+
+    st.write(home2_temp, unsafe_allow_html=True)
+
+    # Create a container to align images side by side
+    st.image("mental-health.jpg", use_column_width=False)
+    st.sidebar.write('Created by. Akash Kumar')
+
+
+def main():
+    st.set_page_config(page_title="Mental Health Treatment Project", page_icon=":chart_with_upwards_trend:",
+                       layout='wide')
+    st.sidebar.title("Navigation")
+    section = st.sidebar.radio("Go to Menu", (
+    "Home", "About Project", "Data Preprocessing & Cleansing", "Exploratory Data Analysis", "Machine Learning"))
+
+    if section == "Home":
+        main_section()
+    elif section == "About Project":
+        st.write(about_temp, unsafe_allow_html=True)
+        st.write(desc_about_temp, unsafe_allow_html=True)
+    elif section == "Data Preprocessing & Cleansing":
+        st.write(prep_temp, unsafe_allow_html=True)
+        ds_dataprep_app()
+    elif section == "Exploratory Data Analysis":
+        st.write(eda_temp, unsafe_allow_html=True)
+        ds_eda_app()
+    elif section == "Machine Learning":
+        st.write(ml_temp, unsafe_allow_html=True)
+        ds_ml_app()
+
+
+if __name__ == "__main__":
+    main()
---
+++ ml_app.py
@@ -0,0 +1,156 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib as jl
+import pickle as pk
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score,precision_score, _dist_metrics, recall_score, fbeta_score
+from sklearn.metrics import classification_report, precision_recall_curve
+from sklearn.metrics import auc, roc_auc_score, roc_curve
+from sklearn.metrics import make_scorer, recall_score, log_loss
+from sklearn.metrics import average_precision_score
+
+
+# Function to preprocess the data and encode categorical variables
+pd.set_option('future.no_silent_downcasting', True)
+
+def preprocess_data(data):
+    data["Gender"] = data["Gender"].replace({'Male': 0, 'Female': 1, 'Other': 2}).astype(int)
+    data["self_employed"] = data["self_employed"].replace({'Yes': 1, 'No': 0}).astype(int)
+    data["family_history"] = data["family_history"].replace({'Yes': 1, 'No': 0}).astype(int)
+    data["work_interfere"] = data["work_interfere"].replace({'Never': 0, 'Rarely': 1, 'Sometimes': 2, 'Often': 3}).astype(int)
+    data["remote_work"] = data["remote_work"].replace({'Yes': 1, 'No': 0}).astype(int)
+    return data
+
+
+# Load the pre-trained machine learning model
+@st.cache_resource
+def load_model(ml_model):
+    if ml_model == 'Logistic Regression':
+        return jl.load('LR_Classifier.pkl')
+    elif ml_model == 'Decision Tree':
+        return jl.load('DT_Classifier.pkl')
+    elif ml_model == 'Random Forest Classifier':
+        return jl.load('RF_Classifier.pkl')
+    elif ml_model == 'KNeighborsClassifier':
+        return jl.load('KNN.pkl')
+    elif ml_model == 'Support Vector Classifier':
+        return jl.load('SVM.pkl')
+    elif ml_model == 'XGBoost Classifier':
+        return jl.load('XGBoost_Classifier.pkl')
+
+
+
+def ds_ml_app():
+    # Add your machine learning content here
+
+    st.write('')
+
+    with st.expander("Modeling and Evaluation"):
+        st.write("Data Preprocessing:")
+        st.write(
+            "To prepare the data for modeling, we first dropped unnecessary columns that were not relevant to our research purpose. The columns dropped include 'Country', 'benefits', 'care_options', 'wellness_program', 'seek_help', 'anonymity', 'leave', 'mental_health_consequence', 'phys_health_consequence', 'coworkers', 'supervisor', 'mental_health_interview', 'phys_health_interview', 'mental_vs_physical', and 'obs_consequence'.")
+        st.write(
+            "Next, we performed manual encoding for categorical variables with more than two values. The following variables were encoded:")
+        st.write(" - 'self_employed': Replaced 'No' with 0 and 'Yes' with 1.")
+        st.write(" - 'family_history': Replaced 'No' with 0 and 'Yes' with 1.")
+        st.write(" - 'treatment': Replaced 'No' with 0 and 'Yes' with 1.")
+        st.write(" - 'remote_work': Replaced 'No' with 0 and 'Yes' with 1.")
+        st.write(" - 'tech_company': Replaced 'No' with 0 and 'Yes' with 1.")
+        st.write(" - 'Gender': Replaced 'Male' with 0, 'Female' with 1, and 'Other' with 2.")
+        st.write(
+            " - 'work_interfere': Replaced 'Never' with 0, 'Rarely' with 1, 'Sometimes' with 2, and 'Often' with 3.")
+        st.write(
+            " - 'no_employees': Replaced categorical ranges with numerical values, e.g., '1-5' with 0, '6-25' with 1, and so on.")
+
+        st.write("Feature Importance:")
+        st.write(
+            "To understand the importance of each feature in predicting the need for mental health treatment, we used mutual information (MI) scores. The MI scores provide insights into how strongly each variable influences the 'treatment' column. The results showed that 'work_interfere' and 'family_history' have the highest MI scores, indicating that they are the most influential variables in determining an employee's mental health treatment needs. On the other hand, 'no_employees' and 'tech_company' had MI scores of 0, suggesting that they do not have any predictive power in this context.")
+
+        st.write("Data Splitting:")
+        st.write(
+            "Before modeling, we split the dataset into dependent and independent variables. The 'treatment' column was designated as the dependent variable, while the rest of the columns were considered independent variables. We used an 80-20 train-test split with stratification to ensure balanced representation of the target variable in both sets.")
+
+        st.write("Feature Scaling:")
+        st.write(
+            "To standardize the independent variables, we performed feature scaling using the StandardScaler. This step is important for models that are sensitive to the scale of input features, such as logistic regression.")
+
+        st.write("Model Evaluation:")
+        st.write(
+            "We evaluated the performance of three different models: Logistic Regression, Decision Tree Classifier, and Random Forest. We used two main evaluation metrics - ROC AUC and Accuracy - to assess each model's predictive capabilities.")
+
+        st.write("Logistic Regression:")
+        st.write(
+            "Despite promising ROC AUC and Accuracy mean scores of 76.75 and 71.72, respectively, we observed issues with this model. When predicting a single sample, the model consistently predicts a treatment value of 1, with probabilities close to 1 for all samples. This behavior indicates that the logistic regression model might be overfitting to the data or encountering convergence issues.")
+
+        st.write("Decision Tree Classifier:")
+        st.write(
+            "The Decision Tree model showed lower ROC AUC and Accuracy mean scores of 64.98 and 63.29, respectively. Additionally, the probabilities for the predicted classes were consistently between 0 and 1, suggesting that the model may not be effectively capturing the underlying patterns in the data.")
+
+        st.write("Random Forest:")
+        st.write(
+            "Despite slightly lower ROC AUC mean score of 70.26, the Random Forest model exhibited better generalization. The probabilities of the predicted classes were not highly polarized, indicating a more balanced and reasonable model output.")
+
+        st.write(
+            "Based on the evaluation results, we have decided to use the Random Forest model for its more reliable and stable predictions, considering the probabilities and overall performance compared to the other models. Further tuning and optimization of the Random Forest model could potentially improve its performance and make it a robust predictor for mental health treatment needs.")
+
+    st.subheader('Mental Health Treatment Prediction')
+    st.write('Enter the following information:')
+
+    # Input fields
+    age = st.number_input('Age', min_value=18, max_value=100, value=30)
+    gender = st.selectbox('Gender', ['Male', 'Female', 'Other'])
+    self_employed = st.selectbox('Self Employed', ['No', 'Yes'])
+    family_history = st.selectbox('Family History of Mental Illness', ['No', 'Yes'])
+    work_interfere = st.selectbox('Mental Illness Interferes with Work', ['Never', 'Rarely', 'Sometimes', 'Often'])
+    remote_work = st.selectbox('Remote Work', ['No', 'Yes'])
+    ml_model = st.selectbox('Machine Learning Model',
+                            ['Random Forest Classifier', 'Logistic Regression', 'Decision Tree Classifier',
+                             'XBoost Classifier', 'KNeighbors Classifier', 'Support Vector Classifier',
+                             'Stacking Classifier'])
+
+    # Convert input to a DataFrame
+    input_data = pd.DataFrame({
+        'Age': [age],
+        'Gender': [gender],
+        'self_employed': [self_employed],
+        'family_history': [family_history],
+        'work_interfere': [work_interfere],
+        'remote_work': [remote_work]
+    })
+
+    # Preprocess the input data
+    input_data_encoded = preprocess_data(input_data)
+
+    model = load_model(ml_model)
+
+    # Make predictions
+    prediction = model.predict(input_data_encoded)
+    prediction_proba = model.predict_proba(input_data_encoded)[:, 1]  # Probability of positive class (treatment needed)
+    prediction_text = "Mental health treatment is needed." if prediction[0] == 1 else "Mental health treatment is not needed."
+
+    # Display the prediction
+    st.subheader('Prediction')
+    st.write(prediction_text)
+
+    # Improve prediction_proba text
+    st.subheader('Prediction Probability')
+    if prediction[0] == 1:
+        st.write(f"There is a {prediction_proba[0] * 100:.2f}% probability that mental health treatment is needed.")
+    else:
+        st.write(f"There is a {prediction_proba[0] * 100:.2f}% probability that mental health treatment is not needed.")
+
+    # Improve prediction_proba text
+    st.subheader('Suggestion')
+    if prediction[0] == 1:
+        st.write(
+            "If you or someone you know is struggling with mental health, it's essential to seek help. Reach out to mental health professionals, support groups, or helplines to get the support you need.")
+        st.write("- Seeking professional help can make a difference -")
+    else:
+        st.write(
+            "Even if mental health treatment is not predicted to be needed, it's crucial to prioritize your mental well-being. Engaging in self-care practices, talking to friends or family, and seeking professional guidance can contribute to a healthier and happier life.")
+        st.write("- Taking care of your mental health is essential for overall well-being -")
+
+
+if __name__ == "__main__":
+    ds_ml_app()
